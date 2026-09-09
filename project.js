2f1df6c53c8e61269674b509e8a557dd201dfbcf
waalidait <snwalid08@gmail.com>
2026-09-09 15:28:53 +0100

    fix error in fun achetter

--- project.js
+++ project.js
@@ -217,18 +217,27 @@ function checkidentifian(){
         if (identifian >= 1 && identifian <= 20 ) {
             return identifian;
         }
-        console.log("il faut ecrir le identifiant")
-      }while(identifian == "")
+        else{
+         console.log("Trajet introuvable.")
+        }
+ 
+       }while(identifian == "" || identifian > 20 || identifian <= 0)
 }
 let id_co = 1;
 function Acheter(){
     let obj = {}
     let name = checkname()
     let identifian = checkidentifian()
+
     let seatnbr ;
     for(let i = 0; i < trips.length;i++){
-        if(trips[i].id == identifian){
-            seatnbr =trips[i].availableSeats
+
+         if(trips[i].availableSeats == 0){
+            console.log("Trajet complet.")
+         }
+
+        else if(trips[i].id == identifian){
+            seatnbr = trips[i].availableSeats
             trips[i].availableSeats -= 1
 
             obj.id = id_co++
@@ -236,10 +245,23 @@ function Acheter(){
             obj.tripId = trips[i].id
             obj.price = trips[i].price
             obj.seatNumber = seatnbr;
-             tickets.push(obj);
-            console.log(tickets);
-            break
+            obj.dest = trips[i].destination
+            obj.arriv = trips[i].departure
+            tickets.push(obj);
+
+            console.log("======Ticket acheté avec succès.====");
+            console.log("=====================================");
+            console.log("");
+            console.log(`ticke#${obj.tripId}`);
+            console.log(`Passager: ${obj.passengerName}`);
+            console.log(`trajet: ${obj.dest} --> ${obj.arriv}`);
+            console.log(`place : ${obj.seatNumber}`)
+            console.log(`prix : ${obj.price}`)
+            console.log("");
+            console.log("=====================================");
+             break
          }
+          
          
     }
      
